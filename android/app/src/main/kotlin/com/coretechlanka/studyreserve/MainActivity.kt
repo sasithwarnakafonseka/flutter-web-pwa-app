56bf9a5385f6c9b4184cda4782e3cc92906c52bc
Warnaka <warnaka@taloslogy.com>
2021-01-16 21:04:42 +0530

    Fiirst git commit

---
+++ android/app/src/main/kotlin/com/coretechlanka/studyreserve/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.coretechlanka.studyreserve
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:studyreserve/connection_status_bar.dart';
+// import 'package:studyreserve/web.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: WebviewScaffold(
+                url: 'https://studyreserve.com/',
+                clearCache: true,
+                appCacheEnabled: true,
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConnectionStatusBar(),
+          ),
+        ],
+      ),
+    );
+  }
+}
